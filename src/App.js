1d747ce0f595e01d07759000310feba7047ca6d3
Samppa Savonen <samppa.savonen@netlight.com>
2017-02-22 14:42:21 +0200

    MainPage and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+
 
 class App extends Component {
   render() {
@@ -9,8 +10,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Lok.it</h2>
-          {/*{this.props.children}*/}
+          
         </div>
+
+        {this.props.children}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +22,8 @@ class App extends Component {
   }
 }
 
-// App.propTypes = {
-//   children: PropTypes.object.isRequired
-// };
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
 
 export default App;
---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class MainPage extends Component{
+    render(){
+        return (
+            <div>
+               <h2>Heijaa</h2>             
+            </div>
+        );
+    }
+}
