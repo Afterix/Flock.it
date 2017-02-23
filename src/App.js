2491d7cbff37cc6f4e666deef3caf78d73fa915e
Marika Mörsky <marika.morsky@netlight.com>
2017-02-23 10:45:04 +0200

    Move components under MainPage from App component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import logo from './logo.svg';
+import MainPage from './components/MainPage.js';
 import './App.css'; 
-import NewMessageBox from './components/NewMessageBox.js';
-import Message from './components/Message.js';
 
 
 class App extends Component {
@@ -12,8 +10,6 @@ class App extends Component {
         <div className="App-header">
           <h2>flock.it</h2>
         </div>
-        <NewMessageBox/> 
-        <Message message="test message" profilePictureSource={logo}/>
         {this.props.children}
       </div>
     );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react";
+import NewMessageBox from './NewMessageBox.js';
+import Message from './Message.js';
+import logo from '../logo.svg';
 
 class MainPage extends Component{
     render(){
         return (
             <div>
-               <h2>Heijaa</h2>             
+                <NewMessageBox />
+                <Message message="test message" profilePictureSource={logo} />
             </div>
         );
     }
