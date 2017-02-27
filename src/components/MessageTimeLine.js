6345b182d6f41a7d34529cea35385bec9033e09a
Marika Mörsky <marika.morsky@netlight.com>
2017-02-27 14:42:00 +0200

    Sort the messages in the timeline in decending order by message timestamp

--- src/components/MessageTimeLine.js
+++ src/components/MessageTimeLine.js
@@ -3,10 +3,15 @@ import Message from './Message.js';
 
 class MessageTimeLine extends Component{
     render(){
-        const messages = this.props.messages.map((message) =>
+        let messages = this.props.messages.map((message) =>
             <Message message={message.text} messageId={message.messageId} 
             timestamp={message.timestamp} userId={message.userId}/>
         );
+
+        //Sort the messages descending order based on timestamp 
+        messages = messages.sort(function(message, other) {
+            return other.props.timestamp.getTime() - message.props.timestamp.getTime();
+        });
 
         return (
             <div>
